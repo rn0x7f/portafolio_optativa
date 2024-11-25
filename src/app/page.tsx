8a01d60fa08914ca13a11ef6077ca66199618953
rn0x7f <ampheriz0@gmail.com>
2024-11-24 22:59:10 -0600

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import OpenBookBackground from './open-book-background'
-
+export const metadata = {
+  title: 'Portafolio - Renato García Morán',
+  description: 'Portafolio para mi optativa final de quinto semestre',
+};
 export default function Home() {
   return <OpenBookBackground />
 }
